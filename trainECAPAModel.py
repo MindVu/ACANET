27218afa5894bd59ac0ce0904b6892942c2bb0db
MindVu <vuducminh1311@gmail.com>
2023-11-07 21:02:45 +0700

    Update trainECAPAModel.py

--- trainECAPAModel.py
+++ trainECAPAModel.py
@@ -18,13 +18,13 @@ parser.add_argument('--lr',         type=float, default=0.001,   help='Learning 
 parser.add_argument("--lr_decay",   type=float, default=0.97,    help='Learning rate decay every [test_step] epochs')
 
 ## Training and evaluation path/lists, save path
-parser.add_argument('--train_list', type=str,   default="/kaggle/working/ECAPA-VLSP23/train_list.txt",     help='The path of the training list')
+parser.add_argument('--train_list', type=str,   default="/kaggle/working/ACANET/train_list.txt",     help='The path of the training list')
 parser.add_argument('--train_path', type=str,   default="/kaggle/input/vlsp-sv-2023/vietnam_celeb",                    help='The path of the training data')
-parser.add_argument('--eval_list',  type=str,   default="/kaggle/working/ECAPA-VLSP23/eval_list.txt",              help='The path of the evaluation list')
+parser.add_argument('--eval_list',  type=str,   default="/kaggle/working/ACANET/eval_list.txt",              help='The path of the evaluation list')
 parser.add_argument('--eval_path',  type=str,   default="/kaggle/input/vlsp-sv-2023/spoofing_data",                    help='The path of the evaluation data')
 parser.add_argument('--musan_path', type=str,   default="/kaggle/input/musan-noise/musan",                    help='The path to the MUSAN set')
 parser.add_argument('--rir_path',   type=str,   default="/kaggle/input/room-impulse-response-and-noise-database/RIRS_NOISES/simulated_rirs",     help='The path to the RIR set');
-parser.add_argument('--save_path',  type=str,   default="/kaggle/working/ECAPA-VLSP23/exps/exp1",                                     help='Path to save the score.txt and models')
+parser.add_argument('--save_path',  type=str,   default="/kaggle/working/ACANET/exps/exp1",                                     help='Path to save the score.txt and models')
 parser.add_argument('--initial_model',  type=str,   default="",                                          help='Path of the initial_model')
 
 ## Model and Loss settings
