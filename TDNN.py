e860255318be4c9a5991d4137bbddfdf096e2940
MindVu <vuducminh1311@gmail.com>
2023-11-08 22:09:42 +0700

    Update TDNN.py

--- TDNN.py
+++ TDNN.py
@@ -25,7 +25,7 @@ class BatchNorm1d(_BatchNorm1d):
     """1D batch normalization. Skip transpose is used to improve efficiency."""
 
     def __init__(self, *args, **kwargs):
-        super().__init__(skip_transpose=False, *args, **kwargs)
+        super().__init__(skip_transpose=True, *args, **kwargs)
 
 
 class TDNNBlock(nn.Module):
