8912b496da89f88149a48616b6329c64ecde6cd0
MindVu <vuducminh1311@gmail.com>
2023-11-08 21:33:55 +0700

    Update model.py

--- model.py
+++ model.py
@@ -260,7 +260,7 @@ class ECAPA_TDNN(nn.Module):
             raise Exception("Check formatting of input")
 
         # Expects x to be in BATCH FIRST format [Batch, Filters, Time]
-        x = x.permute(0, 1, 2)
+        x = x.permute(1, 2, 0)
         x = self.ch_expansion(x)  # perform channel expansion before anything else
 
         # First we expand our latent query matrix to size of batch
